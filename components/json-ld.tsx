69c7514266107f044dc974468f14cebaa00d6591
머스트 <hyenlient@gmail.com>
2026-06-28 00:03:43 +0900

    feat(seo): add JsonLd render component

---
+++ components/json-ld.tsx
@@ -0,0 +1,9 @@
+// components/json-ld.tsx
+export function JsonLd({ data }: { data: object | object[] }) {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}
